eeb260b0c8ddee631e160f2f4488709d73a401c2
Gameboi9000 <Kande0234@stu.hostos.cuny.edu>
2024-02-11 17:41:58 -0500

    added more movement

--- Assets/Script/Hazard.cs
+++ Assets/Script/Hazard.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Hazard : MonoBehaviour
 {
 
-    public Transform hazard_0,hazard_2, hazard_4, hazard_6;     
+    public Transform hazard_0,hazard_1, hazard_3, hazard_5,hazard_2, hazard_4, hazard_6, boardHazard_0, winningTile, world_art;     
     
     Vector3 startPos; //inital position of the player
 
@@ -38,6 +38,32 @@ public class Hazard : MonoBehaviour
             Debug.Log("I'm in the position -HAZARD_6");
             transform.position = startPos; 
         }
+         else if (transform.position == hazard_1.position) {
+            Debug.Log("I'm in the position -HAZARD_1");
+            transform.position = startPos; 
+        }
+         else if (transform.position == hazard_3.position) {
+            Debug.Log("I'm in the position -HAZARD_3");
+            transform.position = startPos; 
+        }
+         else if (transform.position == hazard_5.position) {
+            Debug.Log("I'm in the position -HAZARD_5");
+            transform.position = startPos; 
+        }
+
+        else if(transform.position == boardHazard_0.position) {
+            Debug.Log("I'm on the BoardHazard_0 position, Will do something bad soon");
+            transform.position = startPos; 
+        }
+        else if(transform.position == winningTile.position) {
+            Debug.Log("I'm on the winning tile position, will go to the next level soon");
+
+        }
+        else if(transform.position == world_art.position) {
+            Debug.Log("player is interacting with world_art instance of the class hazard");
+
+        }
+    
     
     }
     }
---
+++ Assets/Script/Hazard_Movement.cs
@@ -0,0 +1,30 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Hazard_Movement : MonoBehaviour
+// {
+//     public Transform Hazard_Movement;
+//     Vector3 startPos; 
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+        
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {  
+//         //1. make the card move  only on the x axis across using a loop
+//         for(i=0; i< Hazard_Movement[i];i++)
+
+
+//         //when moving if the enemy hit a target reset that target position
+//         if (transform.position == Hazard_Movement.position) {
+//             Debug.Log("I'm hit by the hazard");
+//             transform.position = startPos; //When we are at the hazard update the position
+//         }
+
+//         //Once I move to the final position, reset the enemy position to loop again
+//     }
+// }
